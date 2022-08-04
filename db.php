bed36b1c5f594eb6812294e449f373dd450a6b02
qadri2311 <110554347+qadri2311@users.noreply.github.com>
2022-08-03 23:55:58 -0800

    delete

--- db.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = mysqli_connect('localhost', 'root', '');
-$database = mysqli_select_db($conn, 'ttest_dexa');
-
-$encodedData = file_get_contents('php://input');  // take data from react native fetch API
-$decodedData = json_decode($encodedData, true);
